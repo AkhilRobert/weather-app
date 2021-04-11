a22a9e57831e0166fcf4b54d91334faefa75c064
Akhil Robert <akhil@Akhils-MacBook-Pro.local>
2021-04-11 16:02:01 +0530

    fixed dynamic routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import { Route } from 'react-router-dom';
-import Index from './pages/index';
+import { Route, Switch } from 'react-router-dom';
+import Index from './pages/index/Index';
 import WeatherDetails from './pages/weather-details/WeatherDetails';
 
 function App() {
   return (
     <>
-      <Route path={'/'}>
-        <Index />
-      </Route>
+      <Switch>
 
-      <Route path={'/:id'}>
-        <WeatherDetails />
-      </Route>
+        <Route path={'/:id'}>
+          <WeatherDetails />
+        </Route>
+
+        <Route path={'/'}>
+          <Index />
+        </Route>
+
+      </Switch>
     </>
   );
 }
